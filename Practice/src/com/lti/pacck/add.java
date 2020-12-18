a94396b104887b58d7b864933b8b290524c2aff7
neha <nehachan4129@gmail.com>
2020-12-18 09:48:19 +0530

    my first commit

---
+++ Practice/src/com/lti/pacck/add.java
@@ -0,0 +1,13 @@
+package com.lti.pacck;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20,c=0;
+		c=a+b;
+		System.out.println(c);
+
+	}
+
+}
