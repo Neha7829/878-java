40bf9fbbf045941de125458664a160edc1b8b8dc
neha <nrhachan4129@gmail.com>
2020-12-18 10:13:57 +0530

    added a function

--- Practice/src/com/lti/pacck/add.java
+++ Practice/src/com/lti/pacck/add.java
@@ -1,9 +1,16 @@
 package com.lti.pacck;
 
 public class add {
+	
+	public void sum1(int x,int y) {
+		int result=0;
+		result=x+y;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		add a1=new add();
+		a1.sum1(90,80);
 		int a=10,b=20,c=0;
 		c=a+b;
 		int sum=0;
